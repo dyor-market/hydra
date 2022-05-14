7b24dedafe13436512b0a05417f9789c9562b3c4
It is I <jarettdunn+git@gmail.com>
2022-05-14 20:42:42 -0300

    hydra

--- programs/hydra/src/utils/validation/mod.rs
+++ programs/hydra/src/utils/validation/mod.rs
@@ -1,4 +1,4 @@
-use crate::error::HydraError;
+use crate::error::ErrorCode;
 use crate::state::{Fanout, MembershipModel};
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::instruction::Instruction;
@@ -9,24 +9,32 @@ pub fn assert_derivation(
     program_id: &Pubkey,
     account: &AccountInfo,
     path: &[&[u8]],
-    error: Option<error::Error>,
-) -> Result<u8> {
+    error: Option<ProgramError>,
+) -> Result<u8, ProgramError> {
     let (key, bump) = Pubkey::find_program_address(&path, program_id);
     if key != *account.key {
         if error.is_some() {
             let err = error.unwrap();
             msg!("Derivation {:?}", err);
-            return Err(err.into());
+            return Err(err);
         }
         msg!("DerivedKeyInvalid");
-        return Err(HydraError::DerivedKeyInvalid.into());
+        return Err(ErrorCode::DerivedKeyInvalid.into());
     }
     Ok(bump)
 }
 
-pub fn assert_owned_by(account: &AccountInfo, owner: &Pubkey) -> Result<()> {
+pub fn assert_owned_by(account: &AccountInfo, owner: &Pubkey) -> ProgramResult {
     if account.owner != owner {
-        Err(HydraError::IncorrectOwner.into())
+        Err(ErrorCode::IncorrectOwner.into())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn assert_owned_by_or_owned_by(account: &AccountInfo, owner: &Pubkey, or_owner: &Pubkey) -> ProgramResult {
+    if account.owner != owner || account.owner != or_owner {
+        Err(ErrorCode::IncorrectOwner.into())
     } else {
         Ok(())
     }
@@ -35,9 +43,9 @@ pub fn assert_owned_by(account: &AccountInfo, owner: &Pubkey) -> Result<()> {
 pub fn assert_membership_model(
     fanout: &Account<Fanout>,
     model: MembershipModel,
-) -> Result<()> {
+) -> Result<(), ProgramError> {
     if fanout.membership_model != model {
-        return Err(HydraError::InvalidMembershipModel.into());
+        return Err(ErrorCode::InvalidMembershipModel.into());
     }
     Ok(())
 }
@@ -46,8 +54,8 @@ pub fn assert_ata(
     account: &AccountInfo,
     target: &Pubkey,
     mint: &Pubkey,
-    err: Option<error::Error>,
-) -> Result<u8> {
+    err: Option<ProgramError>,
+) -> Result<u8, ProgramError> {
     assert_derivation(
         &anchor_spl::associated_token::ID,
         &account.to_account_info(),
@@ -60,9 +68,9 @@ pub fn assert_ata(
     )
 }
 
-pub fn assert_shares_distributed(fanout: &Account<Fanout>) -> Result<()> {
+pub fn assert_shares_distributed(fanout: &Account<Fanout>) -> Result<(), ProgramError> {
     if fanout.total_available_shares != 0 {
-        return Err(HydraError::SharesArentAtMax.into());
+        return Err(ErrorCode::SharesArentAtMax.into());
     }
     Ok(())
 }
@@ -71,18 +79,18 @@ pub fn assert_holding(
     owner: &AccountInfo,
     token_account: &Account<TokenAccount>,
     mint_info: &AccountInfo,
-) -> Result<()> {
+) -> Result<(), ProgramError> {
     assert_owned_by(mint_info, &spl_token::id())?;
     let token_account_info = token_account.to_account_info();
     assert_owned_by(&token_account_info, &spl_token::id())?;
     if token_account.owner != *owner.key {
-        return Err(HydraError::IncorrectOwner.into());
+        return Err(ErrorCode::IncorrectOwner.into());
     }
     if token_account.amount < 1 {
-        return Err(HydraError::WalletDoesNotOwnMembershipToken.into());
+        return Err(ErrorCode::WalletDoesNotOwnMembershipToken.into());
     }
     if token_account.mint != mint_info.key() {
-        return Err(HydraError::MintDoesNotMatch.into());
+        return Err(ErrorCode::MintDoesNotMatch.into());
     }
     Ok(())
 }
@@ -91,9 +99,9 @@ pub fn assert_distributed(
     ix: Instruction,
     subject: &Pubkey,
     membership_model: MembershipModel,
-) -> Result<()> {
+) -> Result<(), ProgramError> {
     if ix.program_id != crate::id() {
-        return Err(HydraError::MustDistribute.into());
+        return Err(ErrorCode::MustDistribute.into());
     }
     let instruction_id = match membership_model {
         MembershipModel::Wallet => [252, 168, 167, 66, 40, 201, 182, 163],
@@ -101,10 +109,10 @@ pub fn assert_distributed(
         MembershipModel::Token => [126, 105, 46, 135, 28, 36, 117, 212],
     };
     if instruction_id != ix.data[0..8] {
-        return Err(HydraError::MustDistribute.into());
+        return Err(ErrorCode::MustDistribute.into());
     }
     if subject != &ix.accounts[1].pubkey {
-        return Err(HydraError::MustDistribute.into());
+        return Err(ErrorCode::MustDistribute.into());
     }
     Ok(())
 }
@@ -112,10 +120,10 @@ pub fn assert_distributed(
 pub fn assert_valid_metadata(
     metadata_account: &AccountInfo,
     mint: &AccountInfo,
-) -> Result<Metadata> {
+) -> Result<Metadata, ProgramError> {
     let meta = Metadata::from_account_info(metadata_account)?;
     if meta.mint != *mint.key {
-        return Err(HydraError::InvalidMetadata.into());
+        return Err(ErrorCode::InvalidMetadata.into());
     }
     Ok(meta)
 }
